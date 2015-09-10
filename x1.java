17de29245eb4e1af68eaeb7b17a44a7354a0582d
lxve-idxl <reid.j.alexander@gmail.com>
2015-09-09 21:54:27 -0400

    Update x1.java

--- x1.java
+++ x1.java
@@ -1,5 +1,5 @@
 //////// I'm trying to add a house, fix the tree, give some shape to my creature a
-//////// What is my name?  (CST 112; today's date?)
+//////// My name is Alex Reid  (CST 112; 9/9/15)
 
 //// GLOBALS:  coordinates, speed, etc.
 float x, y;       // Position of creature.
@@ -27,16 +27,17 @@ void draw() {
   rect( 0, horizon, width, height*3/4 );      // grass.
 
   /* INSERT YOUR CODE HERE! */
-  fill(120, 100, 100);
-  rect(300, 60, 60, 60 ); //house
-  triangle(300, 60, 330, 30, 360, 60 ); //roof
-  fill( 100, 200, 100 );
-  triangle( 130,80, 160, horizon-100, 190, 80  );  // tree
-  fill( 150, 50, 0);
-  rect( 136, horizon-50, 28, 50);
+  fill(120, 100, 100); rect(300, 60, 60, 60 ); //house
+  fill(255);
+  rect(323, horizon-30, 15, 30); 
+  triangle(300, 60, 330, 30, 360, 60 ); 
+  fill( 100, 200, 100 ); //roof
+  triangle( 130,80, 160, horizon-100, 190, 80  );  fill( 100, 200, 100 ); //tree
+  triangle( 130,60, 160, horizon-100, 190, 60  );  fill( 150, 50, 0); //tree2.
+  rect( 153, horizon-40, 13, 40);
 
   fill(0);
-  text( "My name is Alex", 10, height-20 );                                          
+  text( "My name is Alex!", 10, height-20 );                                          
 
   //// ACTION:  move (x,y) coordinates.
   x=  x + dx;
@@ -45,11 +46,12 @@ void draw() {
   //// SHOW:  display the creature at (x,y)
 
   /* INSERT YOUR CODE HERE! */
-  fill(255, 0, 0); 
-  ellipse( x, y, 60, 60 );        /* REPLACE THIS WITH YOUR OWN CODE! */
-  ellipse( x+25, y-40, 20, 20);
+  fill(230, 0, 0); 
+  rect( x, y, 30, 45 );        /* REPLACE THIS WITH YOUR OWN CODE! */
   fill(0);
-  text( "MOOP", x, y );
+  ellipse( x+15, y-12, 25, 25);
+  fill(0);
+  text( "Aoi", x+5, y+25 );
 }
 
 
